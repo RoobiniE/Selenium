dc0e7388774e37bea54fb27a5b1b1f18b7c42f52
RoobiniE <124458367+RoobiniE@users.noreply.github.com>
2023-02-04 18:22:23 +0000

    ruby-003

---
+++ firstweek/dayOne/Mobile.java
@@ -0,0 +1,22 @@
+package firstweek.dayOne;
+
+public class Mobile {
+    public static void main (String[] args) throws java.lang.Exception{
+    String mobileBrandName = "Apple";
+    char mobileLogo = 'A';
+    short noOfMobilePiece = 5;
+    int modelNumber = 1323;
+    long mobileMeiNumber = 1234567892l;
+    float mobilePrice = 23532.32f;
+    boolean isDamaged = true;
+    System.out.println("MobileBrandName-"+mobileBrandName);
+    System.out.println("MobileLogo-"+mobileLogo);
+    System.out.println("No of Mobile Piece-"+noOfMobilePiece);
+    System.out.println("ModelNumber-"+modelNumber);
+    System.out.println("MobileMeiNumber-"+mobileMeiNumber);
+    System.out.println("MobilePrice-"+mobilePrice);
+    System.out.println("isDamaged ?-"+isDamaged);
+
+}
+    
+}
